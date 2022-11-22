2be4fec02aa172e37ec2cb99a6f60e0a17f93418
Colin Steidtmann <colinsteidtmann@berkeley.edu>
2022-11-21 23:04:07 -0800

    wrote my name

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
+  let pages = [{ title: "about", url: "/about" }, { title: "resume", url: "/resume" }];
+  pages = [];
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="grid h-screen place-items-center">
+      <div className="font-mono text-center">
+        <h1 className="text-2xl tracking-[0.1em]">
+          colin steidtmann
+        </h1>
+        <div className="flex flex-col">
+          {pages.map((page) => {
+            return <Link className="text-lg" href={page.url}>{page.title}</Link>;
+          })}
+        </div>
+      </div>
+    </div>
   );
 }
