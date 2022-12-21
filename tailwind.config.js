cd413dde1a766f5f22a31a1912dc7fe1b46337e7
Colin Steidtmann <colinsteidtmann@berkeley.edu>
2022-12-20 17:12:33 -0800

    gray-matter tailwindTypography about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    './content/**/*.mdx',
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-      }
-    },
-  },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 };
